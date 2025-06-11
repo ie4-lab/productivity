9faf26499f412938453e9f688fdb1133380af4b8
EC2 Default User <ec2-user@ip-192-168-1-109.ap-northeast-1.compute.internal>
2025-06-11 05:33:16 +0000

    test

---
+++ main.tf
@@ -0,0 +1,14 @@
+module "name" {
+  source = "../../modules"
+    env  = "iac"
+    name = "name"
+
+    vpc_cidr = "10.0.0.0/16"
+    pri_subnet_count = "3"
+
+    desired_size = "3"
+    max_size = "3"
+    min_size = "1"
+    eks_ami_type = "AL2_x86_64"
+    eks_instance_types = "t3.large"
+}
